b6663d2613180dd0bae246ccce700abf1930ac62
444-Tess <tnyamatura84@gmail.com>
2025-10-19 06:16:28 -0700

    Add posts migrations

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -37,12 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-#Third-party apps
     'rest_framework',
     'rest_framework.authtoken',
-
-#Local apps
     'accounts',
     'posts',
 ]
